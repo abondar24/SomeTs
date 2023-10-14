44c49e5590442998e7c37318b78e4126193b3f5f
Alex Bondar <abondar1992@gmail.com>
2023-10-14 12:49:10 +0200

    variables

---
+++ SomeTs/README.md
@@ -0,0 +1,9 @@
+#SomeTS
+
+Basic stuff related to typescript
+
+
+
+## Examples
+
+- vars - how variables in typescript work.
---
+++ SomeTs/vars.ts
@@ -0,0 +1,16 @@
+let greet: string = "Hiiii";
+
+console.log(greet.toUpperCase());
+
+let userId: number = 12345
+console.log(userId);
+console.log(userId.toExponential());
+
+let isHere: boolean = false;
+console.log("Bool:" + isHere);
+
+let test = 2;
+console.log(test.valueOf());
+
+
+export{};
